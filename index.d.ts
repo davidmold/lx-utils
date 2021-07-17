b6278d1d9df96d752e892d3f400392917694a5e3
David Mold <davidmold@gmail.com>
2021-07-17 19:00:07 -0400

    Added index.d.ts made by typescript

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export function waitfor(len: any): any;
+export function getFileAsDataUrl(file: any): Promise<any>;
+export function getDataUrlAsJpegFile(dataUrl: any, width: any, filename: any): Promise<any>;
+export function getDataUrlAsGifFile(dataURI: any, filename: any): File;
+export function getMonths(): string[];
--- index.js
+++ index.js
@@ -10,6 +10,72 @@ function waitfor (len) {
   })
 }
 
+async function getFileAsDataUrl(file) {
+  const reader = new FileReader()
+  return new Promise((resolve, reject) => {
+    reader.onerror = error => {
+      reject(error)
+    }
+    reader.onload = event => {
+      resolve(event.target.result)
+    }
+    reader.readAsDataURL(file)
+  })
+}
+
+async function getDataUrlAsJpegFile(dataUrl, width, filename) {
+  const img = new Image()
+  return new Promise((resolve, reject) => {
+    img.onerror = (err) => {
+      reject(err)
+    }
+    img.onload = () => {
+      const elem = document.createElement('canvas')
+      if(img.width < width) {
+        width = img.width
+      }
+      const sf = width / img.width
+      let height = Math.ceil(img.height * sf)
+      elem.width = width
+      elem.height = height
+      
+      const ctx = elem.getContext('2d')
+      ctx.drawImage(img, 0, 0, width, height)
+      ctx.canvas.toBlob((blob) => {
+        const file = new File([blob], filename, {
+          type: 'image/jpeg',
+          lastModified: Date.now()
+        })
+        resolve(file)
+      }, 'image/jpeg', 0.9)
+    }
+    img.src = dataUrl
+  })
+}
+
+
+
+function getDataUrlAsGifFile(dataURI, filename) {
+  let wh = dataURI.indexOf(',')
+  let str = dataURI.slice(wh + 1)
+  let byteString = atob(str)
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new File([ab], filename, {
+    type: 'image/gif',
+    lastModified: Date.now()
+  })
+}
+
+function getMonths() {
+  return ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
+}
+
 module.exports = {
-  waitfor
+  waitfor, getFileAsDataUrl, getDataUrlAsJpegFile, getDataUrlAsGifFile, getMonths
 }
